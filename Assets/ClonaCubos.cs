7367f8ed7c187f19baaacbfe8f8ec20241650e07
NicoAscu <nicolasascune123@gmail.com>
2025-05-16 10:36:40 -0300

    termine

---
+++ Assets/ClonaCubos.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClonaCubos : MonoBehaviour
+{
+
+    public GameObject cuboPreFab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ClonarCubos()
+    {
+        Instantiate(cuboPreFab);
+    }
+
+}
